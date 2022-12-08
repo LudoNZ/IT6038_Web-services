a836354169394ac99ebede03463c9cd5f3580d7d
ludo <ludobourneville@gmail.com>
2022-12-09 09:48:53 +1300

    Step 3. Register Users Route in index.js

--- practical task/index.js
+++ practical task/index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL;
 const routes = require('./routes/routes')
+const users = require('./routes/users');
+const Joi = require('joi');
 
 mongoose.connect(mongoString);
 const database = mongoose.connection;
@@ -18,8 +20,8 @@ database.once('connected', () => {
 const app = express();
 
 app.use(express.json());
-
 app.use('/api', routes)
+app.use('/api/users', users);
 
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`)
