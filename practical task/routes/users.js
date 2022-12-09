ed38e566878718bf98d4c4688dd98a9c93d8fd9a
ludo <ludobourneville@gmail.com>
2022-12-09 13:01:04 +1300

    Using Lodash To Simplify Our Code

--- practical task/routes/users.js
+++ practical task/routes/users.js
@@ -2,6 +2,7 @@ const { User, validate } = require('../models/user');
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const _ = require('lodash');
 
 router.post('/', async (req, res) => {
     // First Validate The Request
@@ -16,15 +17,11 @@ router.post('/', async (req, res) => {
         return res.status(400).send('That user already exists!');
     } else {
         // Insert the new user if they do not exist yet
-        user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        });
+        user = new User(_.pick(req.body, ['name', 'email', 'password']));
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(user.password, salt);
         await user.save();
-        res.send(user);
+        res.send(_.pick(user, ['_id', 'name', 'email']));
     }
 });
 
