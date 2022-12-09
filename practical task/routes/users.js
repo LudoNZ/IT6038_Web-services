2f7ab7405452ef28861ed1f94b1f4589dde2d969
ludo <ludobourneville@gmail.com>
2022-12-09 21:50:01 +1300

    ACTUALLY Setting Response Headers

--- practical task/routes/users.js
+++ practical task/routes/users.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { User, validate } = require('../models/user');
 const express = require('express');
 const router = express.Router();
@@ -21,7 +23,8 @@ router.post('/', async (req, res) => {
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(user.password, salt);
         await user.save();
-        res.send(_.pick(user, ['_id', 'name', 'email']));
+        const token = jwt.sign({ _id: user._id }, config.get('PrivateKey'));
+        res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
     }
 });
 
